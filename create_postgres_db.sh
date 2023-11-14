b118239c57f884ff4665c730dafc816e4a2c979a
John Callaway <john.callaway@.nih.gov>
2023-11-14 14:49:52 -0500

    Now connecting to postgres DB on the host OS.

---
+++ create_postgres_db.sh
@@ -0,0 +1,2 @@
+java -jar fhir-persistence-schema-5.1.1-cli.jar --db-type postgresql --prop-file postgres.properties --schema-name fhirdata --update-schema --grant-to fhirserver --pool-size 1
+java -jar fhir-persistence-schema-5.1.1-cli.jar --db-type postgresql --prop-file postgres.properties --schema-name fhirdata --update-schema --grant-to fhirserver --pool-size 1
