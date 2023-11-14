bef1b5526d948cec9122b88554c7f66d094a1454
John Callaway <john.callaway@.nih.gov>
2023-11-14 12:59:19 -0500

    Runs Linux Health FHIR server in a docker container.
    TODO: set up persistence.

---
+++ docker/build_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+docker build -t sec_poc/fhir .
+
+# Uncomment the following line and modify the first part of the -v argument to
+# point to where your sec_poc repo is checked out.  Then that directory will
+# be available in the container based on the second part of the argument.
+#docker run -v ~/Dev/CTRP/sec_poc:/opt/R/sec_poc -p 8888:8888 sec_poc/r
+docker run -p 9443:9443 sec_poc/fhir
