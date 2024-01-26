ab58bc8388900f36e3e71fb8a48e246a992418b5
John Callaway <john.callaway@.nih.gov>
2024-01-26 15:53:44 -0500

    Modified schema.

--- interoperability_adapter/umls_etl/schema.sql
+++ interoperability_adapter/umls_etl/schema.sql
@@ -1,15 +1,34 @@
---TODO: rename schema once this is finalized.
---create schema jcallaway;
+--create schema co;
 
-drop table if exists jcallaway.umls_to_ncit;
-create table jcallaway.umls_to_ncit(
+--In general, there is a many-to-many relationship between UMLS IDs (CUIs)
+--and codes in other schemes, so there may be multiple rows in these tables
+--with the same cui or the same code in another scheme.
+
+--CREATE TYPE co.status as enum ('exact_match', etc);
+
+drop table if exists co.umls_ontologies;
+create table co.umls_ontologies(
     id serial primary key,
-    ncit_code character varying(64),
-    umls_code character varying(64),
-    other_code character varying(64),
-    other_description text,
-    other_scheme character varying(64)
+    umls_cui character varying(16) not null,
+    is_umls_preferred boolean not null,         --is CUI UMLS preferred concept or synonym?
+    scheme character varying(64) not null,
+    code character varying(64) not null,
+    is_other_scheme_preferred boolean not null, --is code preferred concept or synonym in other scheme?
+    description text
 );
-create index umls_to_ncit_ncit_code_idx on jcallaway.umls_to_ncit(ncit_code);
-create index umls_to_ncit_other_code_idx on jcallaway.umls_to_ncit(other_code);
-create index umls_to_ncit_other_scheme_idx on jcallaway.umls_to_ncit(other_scheme);
+create index umls_ontologies_cui_idx on co.umls_ontologies(umls_cui);
+create index umls_ontologies_scheme_idx on co.umls_ontologies(scheme);
+create index umls_ontologies_code_idx on co.umls_ontologies(code);
+create unique index umls_ontologies_cui_code on co.umls_ontologies(umls_cui, code);
+
+drop table if exists co.umls_ncit;
+create table co.umls_ncit(
+    id serial primary key,
+    umls_cui character varying(16),
+    is_umls_preferred boolean not null,    --is CUI UMLS preferred concept or synonym?
+    ncit_code character varying(16),
+    is_ncit_preferred boolean not null     --is ncit_code preferred concept or synonym?
+);
+create index umls_ncit_cui_idx on co.umls_ncit(umls_cui);
+create index umls_ncit_code_idx on co.umls_ncit(ncit_code);
+create unique index umls_ncit_cui_code on co.umls_ncit(umls_cui, ncit_code);
